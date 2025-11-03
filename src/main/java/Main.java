9a3198d25d4e2badc7e054ab2eecce45dd5a3e68
Adi-Codes-well <jhaa27211@gmail.com>
2025-11-04 00:13:44 +0530

    Type to execute files using path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
         while (true) {
             System.out.print("$ ");
-            Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             String[] commands = input.split(" ");
+
             switch (commands[0]) {
                 case "exit":
                     System.exit(Integer.parseInt(commands[1]));
@@ -40,9 +43,20 @@ public class Main {
         String[] validCommands = {"exit", "type", "echo"};
         if (search(validCommands, input[1])) {
             System.out.println(input[1] + " is a shell builtin");
-        } else {
-            System.out.println(input[1] + ": not found");
+            return;
         }
+        String path = System.getenv("PATH");
+        String[] dirs = path.split(":");
+
+        for (String dir : dirs) {
+            File file = new File(dir, input[1]);
+            if (file.exists() && file.canExecute()) {
+                System.out.println(input[1] + " is " + file.getAbsolutePath());
+                return;
+            }
+        }
+            System.out.println(input[1] + ": not found");
+
     }
     static boolean search(String[] input, String command) {
         for (String s : input) {
