38b3e53f0a6234e2bc2ff140c343df09d9aba174
Adi-Codes-well <jhaa27211@gmail.com>
2025-11-03 23:58:08 +0530

    Type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,21 +2,57 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
+        while (true) {
+            System.out.print("$ ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            String[] commands = input.split(" ");
+            switch (commands[0]) {
+                case "exit":
+                    System.exit(Integer.parseInt(commands[1]));
+                    break;
+                case "echo":
+                    echo(commands);
+                    break;
+                case "type":
+                    type(commands);
+                    break;
+                default:
+                    printCNF(commands[0]);
+                    break;
+            }
+        }
+    }
 
-         Scanner scanner = new Scanner(System.in);
-         while (true){
-             System.out.print("$ ");
-             String command = scanner.nextLine();
-             if (command.equals("exit 0")) break;
-             else if (command.startsWith("echo")) {
-                 System.out.println(command.substring(5));
-             }
-             else {
-             System.out.println(command + ": command not found");
-             }
+    static void echo(String[] input) {
+        boolean counter = false;
+        for (int i = 1; i < input.length; i++) {
+            if (counter) {
+                System.out.print(" ");
+            }
+            System.out.print(input[i]);
+            counter = true;
+        }
+        System.out.println();
+    }
 
-         }
-
+    static void type(String[] input) {
+        String[] validCommands = {"exit", "type", "echo"};
+        if (search(validCommands, input[1])) {
+            System.out.println(input[1] + " is a shell builtin");
+        } else {
+            System.out.println(input[1] + ": not found");
+        }
+    }
+    static boolean search(String[] input, String command) {
+        for (String s : input) {
+            if (s.equals(command)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    static void printCNF(String input) {
+        System.out.println(input + ": command not found");
     }
 }
