c435db06326b9e4b9d610407f4356cfcb6b1fcc6
Adi-Codes-well <jhaa27211@gmail.com>
2025-11-03 23:39:50 +0530

    Exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ public class Main {
          while (true){
              System.out.print("$ ");
              String command = scanner.nextLine();
+             if (command.equals("exit 0")) break;
              System.out.println(command + ": command not found");
          }
 
