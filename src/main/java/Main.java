0622cc8b38482770b308c1ed9ba56dc8d25bdd49
Adi-Codes-well <jhaa27211@gmail.com>
2025-11-05 13:00:06 +0530

    Append stdout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -161,7 +161,7 @@ static File currentDir = new File(System.getProperty("user.dir"));
                     if (append && outFile != null) {
                         // APPEND mode
                         pb.redirectOutput(ProcessBuilder.Redirect.appendTo(new File(outFile)));
-                        pb.redirectError(new File("/dev/null"));
+                        pb.redirectError(ProcessBuilder.Redirect.INHERIT);
                     }
                     else if (redirect && outFile != null) {
                         // OVERWRITE mode
